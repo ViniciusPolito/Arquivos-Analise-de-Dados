531b45188bb7fed154b524811ab4b2f650396d83
Vinibarpo <41132563+ViniciusPolito@users.noreply.github.com>
2020-12-01 16:57:03 -0300

    Add files via upload
    Funções da Introdução ao R

---
+++ IntroduçãoR.R
@@ -0,0 +1,35 @@
+getwd()
+
+setwd("C:/Users/Pc/Desktop/R")
+
+install.packages("seqinr")
+
+library("seqinr")
+
+aux1 <- 10
+
+aux2 <- 15
+
+aux3 <- 5
+
+ls()
+
+rm(list=ls())
+
+exp(1)
+exp(log(5))
+sin(pi/2)
+cos(pi/2)
+max(2,9,3,6)
+min(4,8,3,2)
+sum(4,2,3,5)
+prod(5,6,8,2)
+sqrt(25)
+factorial(5)
+
+help(mean)#documentação
+?mean #documentação
+example(mean)#exemplos
+help.search("mean")#documentação
+help.start()#documentação
+
